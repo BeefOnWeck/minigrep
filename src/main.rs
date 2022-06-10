f6830a93bfcc7a7ddec0a389f5c32095571320bb
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2022-06-10 06:02:12 -0400

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 
 use std::env;
+use std::process;
 use std::fs;
+use std::error::Error;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(filename)
-        .expect("There was a problem reading the file.");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With contents:\n{}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough input arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
