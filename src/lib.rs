7b2bab2465dcc48afc8c8f2dc1219c6238c1f853
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2022-06-12 14:37:29 -0400

    Updating search functions with iterator adaptors.

--- src/lib.rs
+++ src/lib.rs
@@ -37,23 +37,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         search_case_insensitive(&config.query, &contents)
     };
 
-  for line in results {
-      println!("{}", line);
-  }
+    for line in results {
+        println!("{}", line);
+    }
 
-  Ok(())
+    Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
@@ -61,15 +56,10 @@ pub fn search_case_insensitive<'a>(
     contents: &'a str
 ) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line)
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
