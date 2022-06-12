fdf395bd188af8beb020f08de2a3a797131ed24a
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2022-06-12 12:22:00 -0400

    Removing use of clone() in configuration constructor by leveraging iterators

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let args: std::env::Args = env::args();
+    let config = Config::new(args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
